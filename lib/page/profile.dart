26769d687bad33483eabee16398a96c38d8e5c92
Sarankumar <kumar2004saran@gmail.com>
2023-12-12 10:35:41 +0530

    new

--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -12,18 +12,18 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile Page'),
+        title: const Text('Profile Page'),
       ),
       body: Padding(
         padding: const EdgeInsets.only(top: 50.0),
         child: Card(
           elevation: 18,
           child: Container(
-            color: Color.fromARGB(205, 236, 236, 236),
+            color: const Color.fromARGB(205, 236, 236, 236),
             width: double.infinity,
             height: 400,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 30.0, left: 10, right: 10),
+            child: const Padding(
+              padding: EdgeInsets.only(top: 30.0, left: 10, right: 10),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
